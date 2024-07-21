e33957bed351ba2170e45f07dc7878030cec6acb
JB <29986350+JBStepan@users.noreply.github.com>
2024-07-21 10:33:18 -0500

    Update base_init.sh

--- base_init.sh
+++ base_init.sh
@@ -10,26 +10,29 @@ passwd admin
 
 printf "${BLUE} [!] Updating system 👆...${NC}\n"
 # This will update and upgrade the system packages
-sudo apt update && apt upgrade -y
+apt update && apt upgrade -y
 
 printf "${BLUE} [!] Installing packages 📦...${NC}\n"
 pkgs=(fail2ban ufw git curl neofetch)
-sudo apt-get -y --ignore-missing install "${pkgs[@]}" 
+apt-get -y --ignore-missing install "${pkgs[@]}" 
 
 # Add Docker's official GPG key:
 printf "${BLUE} [!] Installing Docker 🐳...${NC}\n"
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 
 printf "${BLUE} [!]Enabling firewall 🔥...${NC}\n"
 # Add more rules in the future
@@ -38,6 +41,8 @@ sudo ufw enable
 
 printf "${BLUE} [!] Configuring SSH 🐚...${NC}\n"
 # Change someconfig in SSH
+touch /etc/ssh/sshd_config.tmp
+
 sed -i -e '/^\(#\|\)PermitRootLogin/s/^.*$/PermitRootLogin no/' /etc/ssh/sshd_config.tmp
 sed -i -e '/^\(#\|\)PasswordAuthentication/s/^.*$/PasswordAuthentication no/' /etc/ssh/sshd_config.tmp
 sed -i -e '/^\(#\|\)KbdInteractiveAuthentication/s/^.*$/KbdInteractiveAuthentication no/' /etc/ssh/sshd_config.tmp
