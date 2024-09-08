7081b8b8f32e435cf88292d2604cdf69db3b6f8b
JB <29986350+JBStepan@users.noreply.github.com>
2024-09-08 14:31:00 -0500

    Update base_init.sh

--- base_init.sh
+++ base_init.sh
@@ -3,17 +3,20 @@
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
+username=""
+password=""
+
 printf "${BLUE} [!] Creating user account 🧑...${NC}\n"
 # This  handles the user account creation
-useradd -m -U -s /bin/bash -G sudo admin
-passwd admin
+useradd -m -U -s /bin/bash -G sudo $username
+echo $username:$password | chpasswd
 
 printf "${BLUE} [!] Updating system ⬆️...${NC}\n"
 # This will update and upgrade the system packages
 apt update && apt upgrade -y
 
 printf "${BLUE} [!] Installing packages 📦...${NC}\n"
-pkgs=(fail2ban ufw git curl neofetch)
+pkgs=(fail2ban ufw git curl)
 apt-get -y --ignore-missing install "${pkgs[@]}" 
 
 # Add Docker's official GPG key:
@@ -53,13 +56,15 @@ sed -i -e '/^\(#\|\)AuthorizedKeysFile/s/^.*$/AuthorizedKeysFile .ssh\/authorize
 
 jbskey="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7xo/5Ig9j+5yv+RriZjBwbSTAguOemmMmbi0Xa6tulWd6+J0+yFpeZmyMszwI+jEazFsF1YWm1X7QKpvEIGR0wUxk0eGC+DacWRbDjLq9pVUnDWMwMG4DBu/s6TgYYzbPTkIKoQM1+OBhLTJLeeW9fbw+Y1XSbfHTQlC1+XHxwbh+M6Ilb+GqQLagpBTr1adi9dWrLx8sMcg7ERw9msCg1iLloiVq70cBSV2sxzCPmxUCyyS+PmufDY9Dhw8hLW52q+EBCkOdJbU83w1HOuSpTnX7VrgjlcwC/XnMkfxBvFqqAQ1RyBk+0WhLtbswsVabIymW1hrcTTYpWrgMuXKl jbstepan@jbstepan.com"
 
-mkdir /home/admin/.ssh
+mkdir /home/$username/.ssh
 
-touch /home/admin/.ssh/authorized_keys
+touch /home/$username/.ssh/authorized_keys
 
-echo $jbskey > /home/admin/.ssh/authorized_keys
+echo $jbskey > /home/$username/.ssh/authorized_keys
 
 sshd -t
 sudo systemctl restart sshd
+
+timedatectl set-timezone America/Chicago
 
 printf "${BLUE} [!] Script finished ✅.${NC}\n"
